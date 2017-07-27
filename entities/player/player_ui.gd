d3a7fe254aca4d2e3addd03f026cff813fd727ae
Ben Hill <benhill.limes@gmail.com>
2017-07-27 10:42:47 +1000

    removed old stuff

--- entities/player/player_ui.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	pass
-
-func update_score(new_score):
-	get_node("Score").set_text(str(new_score))
--- systems/score_manager/score_manager.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-signal score(value)
-
-func score_received(score):
-	emit_signal("score", score)
