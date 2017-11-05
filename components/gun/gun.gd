5b47a089f05bbfd1db608dcb2e22b258d5e4f24f
Ben Hill <benhill.limes@gmail.com>
2017-11-05 21:51:41 +1000

    modified gun shoot triggers

--- components/gun/gun.gd
+++ components/gun/gun.gd
@@ -11,22 +11,33 @@ var bullet_scene
 
 onready var bullet_container = get_node("Bullets")
 onready var timer_node = get_node("RateOfFire")
-onready var sound_node = get_node("SamplePlayer2D")
+
+var is_firing = false # is the gun currently firing
+var refire_available = true # is the gun ready to fire the next bullet
+var facing_dir = Vector2(0,1) # the direction that the gun is aimed at
 
 func _ready():
 	bullet_scene = load(bullet_scene_path)
 	timer_node.set_wait_time(1.0 / rof)
+	set_fixed_process(true)
 
-func fire_bullet(facing_dir):
-	if (timer_node.get_time_left() == 0):
-		# bullet creation
-		var new_bullet = bullet_scene.instance()
-		new_bullet.direction = facing_dir
-		new_bullet.set_pos(get_global_pos())
-		new_bullet.target_group = target_group
-		new_bullet.movement_speed = bullet_speed
-		bullet_container.add_child(new_bullet)
-		# start timer
+func _fixed_process(delta):
+	# if gun is_firing and refire_available, then fire a bullet
+	if (is_firing && refire_available):
+		fire_bullet()
+		refire_available = false
 		timer_node.start()
-		# audio
-		sound_node.play("bullet")
+	
+
+func fire_bullet():
+	print("Bullet fired")
+	# bullet creation
+#	var new_bullet = bullet_scene.instance()
+#	new_bullet.direction = facing_dir
+#	new_bullet.set_pos(get_global_pos())
+#	new_bullet.target_group = target_group
+#	new_bullet.movement_speed = bullet_speed
+#	bullet_container.add_child(new_bullet)
+
+func _on_RateOfFire_timeout():
+	refire_available = true
